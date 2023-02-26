1e34185042b9efb5a19a2ab3aaaee5852161cef2
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2023-02-26 13:05:38 +0500

    Initial Commit

---
+++ ChatMessage/ChatMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMessageApp.swift
+//  ChatMessage
+//
+//  Created by Benji Loya on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
